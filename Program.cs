60e71eaa598f36739c998e9d17695c861d0d486c
AB <meteoritus75@gmail.com>
2024-04-22 23:52:13 +0200

    Challenge List

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿
+using System.Runtime.InteropServices.Marshalling;
+
+namespace List;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+       List<string> fruits = new List<string>();
+       fruits.Add("fraises");
+       fruits.Add("pommes");
+       fruits.Add("bananes");
+       
+
+       fruits.RemoveAt(0);
+       fruits.Remove("bananes");
+       fruits[0] = "cerises";
+       
+       Console.WriteLine(fruits[0]);
+       Console.WriteLine(fruits.Count);
+    
+
+        
+    }
+}
